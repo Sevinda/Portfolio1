412effd4deae84f78c3f2460451c6d0078f8275b
Sevinda Perera <sevindacharukaperera@gmail.com>
2023-11-10 18:36:32 +0530

    Finalized the hero page

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -2,8 +2,17 @@ import Spline from "@splinetool/react-spline";
 
 const Hero = () => {
   return (
-    <div className="h-[calc(100vh-90px)] flex justify-center items-center flex-col">
+    <div className="h-[calc(100vh-90px)] flex justify-center items-center flex-col relative">
       <Spline scene="https://prod.spline.design/oKHVcueC4mxMTRNg/scene.splinecode" />
+      <div className="absolute right-[150px] flex flex-col items-center">
+        <div className="mb-[50px]">
+          <h1 className="text-[70px] font-medium">Sevinda Perera</h1>
+          <p className="text-[25px]">Software Engineering Undergraduate</p>
+        </div>
+        <p className="bg-neutral-900 py-[10px] px-[40px] text-center rounded-full w-fit flex justify-center items-center">
+          Click and Drag the model to move.
+        </p>
+      </div>
     </div>
   );
 };
